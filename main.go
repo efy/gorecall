b85f923bcf8f58c4d27b096bf20482da1ad1beb1
Shaun Dowling <shaun.dowling@gmail.com>
2017-11-10 14:32:01 +0000

    Stub core routes and handlers

--- main.go
+++ main.go
@@ -3,20 +3,56 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
+	"net/http/fcgi"
+	"os"
+
+	"github.com/gorilla/mux"
 )
 
 var (
-	fcgi = flag.Bool("fcgi", false, "Use Fast CGI")
-	addr = flag.String("addr", ":8080", "Bind address")
+	usefcgi = flag.Bool("fcgi", false, "Use Fast CGI")
+	addr    = flag.String("addr", ":8080", "Bind address")
 )
 
 func main() {
 	flag.Parse()
 
-	if *fcgi {
-		fmt.Println("Run FastCGI server")
-		return
+	r := mux.NewRouter()
+
+	// Routes
+	r.HandleFunc("/", HomeView)
+	r.HandleFunc("/bookmarks", BookmarksView)
+	r.HandleFunc("/import", ImportView)
+	r.HandleFunc("/login", LoginView)
+
+	var err error
+
+	if *usefcgi {
+		err = fcgi.Serve(nil, r)
+	} else {
+		err = http.ListenAndServe(*addr, r)
 	}
 
-	fmt.Println("Run http server", *addr)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func LoginView(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "<h1>Login</h1>")
+}
+
+func ImportView(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "<h1>Import</h1>")
+}
+
+func BookmarksView(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "<h1>Bookmarks</h1>")
+}
+
+func HomeView(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "<h1>Recall</h1>")
 }
