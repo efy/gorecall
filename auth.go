409ccf70b0c6e1d32dde72d9f2db05ab3fb15139
Shaun Dowling <shaun.dowling@gmail.com>
2017-11-14 19:39:59 +0000

    Add wrapper functions for generating and checking password hashes

---
+++ auth.go
@@ -0,0 +1,12 @@
+package main
+
+import "golang.org/x/crypto/bcrypt"
+
+func CheckPasswordHash(password string, hash string) bool {
+	return true
+}
+
+func HashPassword(password string) (string, error) {
+	h, err := bcrypt.GenerateFromPassword([]byte(password), 0)
+	return string(h), err
+}
---
+++ auth_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestCheckPasswordHash(t *testing.T) {
+	password := "test"
+	hash, err := HashPassword(password)
+	if err != nil {
+		t.Error(err)
+	}
+	match := CheckPasswordHash(password, hash)
+	if match != true {
+		t.Error("expected", true, "got", "match")
+	}
+}
+
+func TestCheckPasswordHashPrecomputed(t *testing.T) {
+	password := "test"
+	hash := "$2a$10$StmyKkvbf4f/XJ9cQDv/IOMfU.7Q8Kzns/fwqIOutOW.52lAwAnAq"
+	match := CheckPasswordHash(password, hash)
+	if match != true {
+		t.Error("expected", true, "got", "match")
+	}
+}
