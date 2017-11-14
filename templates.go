5f23259bc50db584e81913c025ae494f5634ffa8
Shaun Dowling <shaun.dowling@gmail.com>
2017-11-14 02:18:15 +0000

    Apply UI framework to templates

--- templates.go
+++ templates.go
@@ -17,19 +17,23 @@ const layouttmpl = `
   </head>
   <body>
     <div class="wrapper">
-      <header>
-        <nav class="top-nav">
-          <div class="nav-left">
-            <a href="/bookmarks">Bookmarks</a>
-            <a href="/import">Import</a>
-            <a href="/login">Login</a>
-          </div>
-          <div class="nav-right">
-            <a href="/bookmarks/new" class="button">New</a>
-          </div>
+      <header class="navbar">
+        <nav class="navbar-section">
+          <a href="/bookmarks" class="btn btn-link">Bookmarks</a>
+          <a href="/import" class="btn btn-link">Import</a>
+          <a href="/login" class="btn btn-link">Login</a>
+        </nav>
+        <div class="navbar-center">
+          Recall
+        </div>
+        <nav class="navbar-section">
+          <a href="/bookmarks/new" class="btn btn-primary">New</a>
         </nav>
       </header>
-      {{ block "content" . }} {{ end }}
+
+      <div class="content-area container">
+        {{ block "content" . }} {{ end }}
+      </div>
     </div>
   </body>
 </html>
@@ -64,7 +68,21 @@ const bookmarkstmpl = `
 
 <h2>Bookmarks</h2>
 
-<table>
+{{ if not . }}
+<div class="empty">
+  <div class="empty-icon">
+    <i class="icon icon-bookmark"></i>
+  </div>
+  <p class="empty-title h5">You have no Bookmarks</p>
+  <p class="empty-subtitle">Choose from the actions below to get started</p>
+  <div class="empty-action">
+    <a href="/import" class="btn btn-primary">Import</a>
+    <a href="/bookmarks/new" class="btn btn-primary">Add</a>
+  </div>
+</div>
+{{ else }}
+
+<table class="table">
   <thead>
     <th>Title</th>
     <th>URL</th>
@@ -82,6 +100,8 @@ const bookmarkstmpl = `
     {{ end }}
   </tbody>
 </table>
+
+{{ end }}
 
 {{ end }}
 `
@@ -108,19 +128,19 @@ const bookmarksnewtmpl = `
 <h2>New Bookmark</h2>
 
 <form action="/bookmarks/new" method="post">
-  <div class="field">
-    <label for="title">Title</label>
-    <input type="text" name="title">
+  <div class="form-group">
+    <label class="form-label" for="title">Title</label>
+    <input id="title" class="form-input" type="text" name="title">
   </div>
 
-  <div class="field">
-    <label for="url">URL</label>
-    <input type="text" name="url">
+  <div class="form-group">
+    <label class="form-label" for="url">URL</label>
+    <input id="url" class="form-input" type="text" name="url">
   </div>
 
-  <div class="field">
-    <button type="submit" class="button-primary">Create</button>
-    <button type="reset" disabled>Reset</button>
+  <div class="form-group">
+    <button type="submit" class="btn btn-primary">Create</button>
+    <button type="reset" class="btn">Reset</button>
   </div>
 </form>
 
@@ -133,13 +153,13 @@ const importtmpl = `
 <h2>Import</h2>
 
 <form method="post" action="/import">
-  <div class="field">
+  <div class="form-group">
     <label for="bookmarks">bookmarks file</label>
     <input type="file" name="bookmarks">
   </div>
 
-  <div class="field">
-    <button type="submit" class="button-primary">Import</button>
+  <div class="form-group">
+    <button type="submit" class="btn-primary">Import</button>
   </div>
 </form>
 
@@ -152,23 +172,25 @@ const logintmpl = `
 <h2>Login</h2>
 
 <form method="post" action="/login">
-  <div class="field">
-    <label for="username">Username</label>
-    <input type="text" name="username">
+  <div class="form-group">
+    <label class="form-label" for="username">Username</label>
+    <input id="username" class="form-input" type="text" name="username">
   </div>
 
-  <div class="field">
-    <label for="password">Password</label>
-    <input type="password" name="password">
+  <div class="form-group">
+    <label class="form-label" for="password">Password</label>
+    <input id="password" class="form-input" type="password" name="password">
   </div>
 
-  <div class="field inline">
-    <input id="remember_me" type="checkbox" name="remember_me">
-    <label for="remember_me">Remember me?</label>
+  <div class="form-group">
+    <label for="remember_me" class="form-checkbox">
+      <input id="remember_me" type="checkbox" name="remember_me">
+      <i class="form-icon"></i>Remember me?
+    </label>
   </div>
 
-  <div class="field">
-    <button type="submit" class="button-primary">Login</button>
+  <div class="form-group">
+    <button type="submit" class="btn btn-primary">Login</button>
   </div>
 </form>
 
