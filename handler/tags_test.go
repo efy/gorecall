7dfbef6aef489c33b8600d8823bfe9b002d7c663
Shaun Dowling <shaun.dowling@gmail.com>
2017-11-30 00:07:52 +0000

    stub handler test

---
+++ handler/tags_test.go
@@ -0,0 +1,17 @@
+package handler
+
+import (
+	"testing"
+)
+
+func TestCreateTagHandler(t *testing.T) {
+}
+
+func TestNewTagHandler(t *testing.T) {
+}
+
+func TestTagHandler(t *testing.T) {
+}
+
+func TestTagsHandler(t *testing.T) {
+}
