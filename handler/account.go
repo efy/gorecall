87849acc5665ccc43838d892820e2ffe47df80df
Shaun Dowling <shaun.dowling@gmail.com>
2017-12-02 16:50:12 +0000

    pass user to account template

--- handler/account.go
+++ handler/account.go
@@ -3,15 +3,33 @@ package handler
 import (
 	"net/http"
 
+	"github.com/efy/gorecall/datastore"
 	"github.com/efy/gorecall/templates"
 )
 
 func (app *App) AccountShowHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		session, err := app.store.Get(r, "sesh")
+		if err != nil {
+			renderError(w, err)
+			return
+		}
+		username, ok := session.Values["username"].(string)
+		if !ok {
+			http.Redirect(w, r, "/login", 302)
+			return
+		}
+		user, err := app.ur.GetByUsername(username)
+		if err != nil {
+			renderError(w, err)
+		}
+
 		templates.RenderTemplate(w, "account.html", struct {
 			Authenticated bool
+			User          *datastore.User
 		}{
 			true,
+			user,
 		})
 	})
 }
