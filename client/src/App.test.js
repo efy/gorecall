e4103b094f82fd29c827fb21758166a867f9975a
Shaun Dowling <shaun.dowling@gmail.com>
2018-05-19 12:40:55 +0100

    frontend: add react testing packages and testing configuration

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer'
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.exists()).toBe(true)
+  });
+
+  it('renders the logo', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  it('renders correctly', () => {
+    const tree = renderer.create(<App />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
